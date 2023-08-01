f2d5372e9c6d13d5c47b21d92d05ff361b677e2d
Manuel Plank <manuel.plank@melli.com>
2023-08-01 09:03:43 +0200

    add cross-compiling support

--- flake.nix
+++ flake.nix
@@ -5,21 +5,31 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
+    android.url = "github:tadfisher/android-nixpkgs";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, android }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [
           (import rust-overlay)
           (self: super: {
-            rustToolchain = super.rust-bin.stable.latest.default;
+            rustToolchain = super.rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+              targets = [ "armv7-linux-androideabi" "aarch64-linux-android" "i686-linux-android" "x86_64-unknown-linux-gnu" ];
+            };
           })
+          (
+            final: prev: {
+              inherit (self.packages.${final.system}) android-sdk android-studio;
+            }
+          )
         ];
+        wavfiylib = "${toString ./.}";
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        wavfiylib = "${toString ./.}/lib";
+        crossPkgs = import nixpkgs { system = "aarch64-linux"; };
       in
       with pkgs;
       {
@@ -28,11 +38,49 @@
             rustToolchain
             openssl
             pkg-config
+            libclang
+            cmake
+            gfortran9
+            openblas
+            crossPkgs.openblas
+            crossPkgs.openssl
+            mold
+            gcc
+            llvmPackages.openmp
           ]);
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
           LD_LIBRARY_PATH = lib.makeLibraryPath [
             wavfiylib
+            oneDNN
+            llvmPackages.openmp
+
           ];
+          shellHook = ''
+            export LIBCLANG_PATH="${pkgs.libclang.lib}/lib"
+            export ANDROID_HOME="${android-sdk}/share/android-sdk"
+            export ANDROID_SDK_ROOT="${android-sdk}/share/android-sdk"
+            export ANDROID_NDK_HOME="${android-sdk}/share/android-sdk/ndk-bundle"
+            export ANDROID_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake"
+            export OPENBLAS_INCLUDE_DIR="${crossPkgs.openblas.dev}/include"
+            export OPENBLAS_LIBRARY="${crossPkgs.openblas}/lib/libopenblas.so"
+            export OPENSSL_LIB_DIR="${crossPkgs.openssl.out}/lib"
+            export OPENSSL_INCLUDE_DIR="${crossPkgs.openssl.dev}/include"
+            export CC="$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android21-clang"
+            export CXX="$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android21-clang++"
+            export JAVA_HOME="${jdk11.home}"
+          '';
+        };
+        packages = {
+          android-sdk = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
+            build-tools-30-0-2
+            cmdline-tools-latest
+            emulator
+            platform-tools
+            platforms-android-3
+            ndk-bundle
+          ]);
+
+          android-studio = pkgs.androidStudioPackages.stable;
         };
       }
     );
