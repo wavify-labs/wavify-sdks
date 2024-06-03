ffa110945f306afce69884e4edf85f8d6c6c150f
Manuel Plank <manuel.plank@melli.com>
2024-06-03 18:04:23 +0200

    refactor rust bindings

--- demos/rust-demo/src/main.rs
+++ demos/rust-demo/src/main.rs
@@ -1,10 +1,9 @@
 use anyhow::Result;
 use env_logger::Env;
 use std::env;
-use std::ffi::CString;
 use std::time::Instant;
 
-use wavify::*;
+use wavify::SttEngine;
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -12,33 +11,19 @@ fn main() -> Result<()> {
     let api_key = env::var("WAVIFY_API_KEY")?;
 
     let (model_path, file_path) = if args.len() < 3 {
-        (
-            "../../models/model-en.bin",
-            // "../../assets/samples_gb1.wav",
-            "../../assets/samples_gb1_small.wav",
-            // "../../assets/samples_gb1_tiny.wav",
-            // "../../assets/english.wav",
-        )
+        ("../../models/model-en.bin", "../../assets/samples_gb0.wav")
     } else {
         (&args[1] as &str, &args[2] as &str)
     };
 
     env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
 
-    unsafe {
-        let model_path_c = CString::new(model_path).expect("CString::new failed");
-        let api_key_c = CString::new(api_key).expect("CString::new failed");
-        let stt_engine = create_stt_engine(model_path_c.as_ptr(), api_key_c.as_ptr());
-
-        let data = from_file(file_path);
-        let now = Instant::now();
-        let raw_result = stt(stt_engine, data);
-        let result = read_result(raw_result);
-        println!("{:?}", result);
-        println!("{:?}", now.elapsed());
-
-        destroy_stt_engine(stt_engine);
-    }
+    let engine = SttEngine::new(model_path, &api_key)?;
+    let now = Instant::now();
+    let result = engine.stt_from_file(file_path)?;
+    println!("{:?}", result);
+    println!("{:?}", now.elapsed());
+    engine.destroy();
 
     Ok(())
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,13 +1,18 @@
-use std::ffi::{CStr, CString};
-use std::os::raw::{c_char, c_float};
-
 use hound::WavReader;
+use std::ffi::{CStr, CString};
+use std::os::raw::c_char;
+
 #[macro_use]
 extern crate log;
 use log::Level;
 
 #[repr(C)]
-pub struct SttEngine {}
+pub struct SttEngine {
+    inner: *mut SttEngineInner,
+}
+
+#[repr(C)]
+struct SttEngineInner;
 
 #[repr(C)]
 pub struct FloatArray {
@@ -15,48 +20,83 @@ pub struct FloatArray {
     pub len: usize,
 }
 
+#[derive(Debug)]
+pub enum WavifyError {
+    InitError,
+    InferenceError,
+}
+
+impl std::fmt::Display for WavifyError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::InitError => write!(f, "Failed to initialize"),
+            Self::InferenceError => write!(f, "Failed to run inference"),
+        }
+    }
+}
+
+impl std::error::Error for WavifyError {}
+
 extern "C" {
-    pub fn create_stt_engine(model_path: *const c_char, api_key: *const c_char) -> *mut SttEngine;
-    pub fn destroy_stt_engine(model: *mut SttEngine);
-    pub fn stt(model: *mut SttEngine, data: FloatArray) -> *mut c_char;
-    pub fn free_result(result: *mut c_char);
+    fn create_stt_engine(model_path: *const c_char, api_key: *const c_char) -> *mut SttEngineInner;
+    fn destroy_stt_engine(model: *mut SttEngineInner);
+    fn stt(model: *mut SttEngineInner, data: FloatArray) -> *mut c_char;
+    fn free_result(result: *mut c_char);
 }
 
-pub fn from_file(filename: &str) -> FloatArray {
-    let mut reader = WavReader::open(filename).unwrap();
-
-    let mut float_data = Vec::new();
-
-    for sample in reader.samples::<i16>() {
-        let float_sample = sample.unwrap() as f64 / i16::MAX as f64;
-        float_data.push(float_sample);
+impl SttEngine {
+    pub fn new(model_path: &str, api_key: &str) -> Result<SttEngine, WavifyError> {
+        let maybe_model_path_c = CString::new(model_path);
+        let maybe_api_key_c = CString::new(api_key);
+        match (maybe_model_path_c, maybe_api_key_c) {
+            (Ok(model_path_c), Ok(api_key_c)) => unsafe {
+                let inner = create_stt_engine(model_path_c.as_ptr(), api_key_c.as_ptr());
+                Ok(SttEngine { inner })
+            },
+            (_, _) => Err(WavifyError::InitError),
+        }
     }
 
-    let data: Vec<f32> = float_data.iter().map(|v| *v as f32).collect();
-    log!(
-        Level::Debug,
-        "Audio codec: {:?} with len: {}",
-        &data[..10],
-        data.len()
-    );
-
-    // data under pointer needs to outlive the function
-    let data_boxed = Box::new(data);
-    let data_ptr = Box::leak(data_boxed.clone()).as_ptr();
-    let data_len = data_boxed.len();
-
-    FloatArray {
-        data: data_ptr,
-        len: data_len,
+    pub fn destroy(self) {
+        unsafe { destroy_stt_engine(self.inner) }
     }
-}
 
-// TODO: Call destroy method after reading
-pub unsafe fn read_result(ffi_result: *mut c_char) -> String {
-    let result_string = unsafe {
-        let c_str = CStr::from_ptr(ffi_result);
-        c_str.to_string_lossy().into_owned()
-    };
-    free_result(ffi_result);
-    result_string
+    pub fn stt(&self, data: &[f32]) -> Result<String, WavifyError> {
+        let float_array = FloatArray {
+            data: data.as_ptr(),
+            len: data.len(),
+        };
+
+        unsafe {
+            let result_ptr = stt(self.inner, float_array);
+            if result_ptr.is_null() {
+                return Err(WavifyError::InferenceError);
+            }
+
+            let result = CStr::from_ptr(result_ptr).to_string_lossy().into_owned();
+            free_result(result_ptr);
+            Ok(result)
+        }
+    }
+
+    pub fn stt_from_file(&self, filename: &str) -> Result<String, WavifyError> {
+        let mut reader = WavReader::open(filename).unwrap();
+
+        let mut float_data = Vec::new();
+
+        for sample in reader.samples::<i16>() {
+            let float_sample = sample.unwrap() as f64 / i16::MAX as f64;
+            float_data.push(float_sample);
+        }
+
+        let data: Vec<f32> = float_data.iter().map(|v| *v as f32).collect();
+        log!(
+            Level::Debug,
+            "Audio codec: {:?} with len: {}",
+            &data[..10],
+            data.len()
+        );
+
+        self.stt(&data)
+    }
 }
