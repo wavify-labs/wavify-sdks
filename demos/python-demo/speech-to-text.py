b0085010b96d6941470a3f073de5fdaa4291314e
Mark Percival <m@mdp.im>
2024-10-09 18:52:29 +0000

    Fix: Better paths, devcontainer, minor Readme fix

--- demos/python-demo/speech-to-text.py
+++ demos/python-demo/speech-to-text.py
@@ -6,12 +6,16 @@ from wavify.stt import SttEngine
 from wavify.utils import LogLevel, set_log_level
 
 set_log_level(LogLevel.DEBUG)
-engine = SttEngine("../../models/model-en.bin", os.getenv("WAVIFY_API_KEY"))
+
+model_path = Path(__file__).parent.parent.parent / "models" / "model-en.bin"
+wav_path = Path(__file__).parent.parent.parent / "assets" / "samples_jfk.wav"
+
+engine = SttEngine(model_path.as_posix(), os.getenv("WAVIFY_API_KEY"))
 
 now = time()
-result_file = engine.stt_from_file(
-    Path(__file__).parent.parent.parent / "assets" / "samples_jfk.wav"
-)
+result_file = engine.stt_from_file(wav_path)
+
 print(result_file)
 print(f"Took {time()-now}s")
+
 engine.destroy()
